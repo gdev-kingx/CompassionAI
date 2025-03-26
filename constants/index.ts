fd53337c35c11be3e2f2082df9f0e2c94f1e53f8
gdev-kingx <marioplayz975@gmail.com>
2025-03-26 21:24:23 +0530

    Init

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+	{
+		id: "about",
+		title: "About Us",
+	},
+	{
+		id: "mission",
+		title: "Our Mission",
+	},
+	{
+		id: "app",
+		title: "Our App",
+	},
+	{
+		id: "contact",
+		title: "Contact Us",
+	},
+];
